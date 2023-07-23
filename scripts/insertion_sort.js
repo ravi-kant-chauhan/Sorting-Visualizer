295f5da4ff6462ad2c0a6ee9ef072239fb87fcd8
Ravi <ravkchauhan1aip@gmail.com>
2023-07-23 17:18:51 +0530

    Update

---
+++ scripts/insertion_sort.js
@@ -0,0 +1,57 @@
+function Insertion()
+{
+    //Setting Time complexities
+    document.getElementById("Time_Worst").innerText="O(N^2)";
+    document.getElementById("Time_Average").innerText="Θ(N^2)";
+    document.getElementById("Time_Best").innerText="Ω(N)";
+
+    //Setting Space complexity
+    document.getElementById("Space_Worst").innerText="O(1)";
+
+    // Setting details
+    document.getElementById("details").innerHTML="Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part."
+
+    const linkElement = document.getElementById("reference")
+    linkElement.setAttribute('href', "https://www.geeksforgeeks.org/insertion-sort/")
+    linkElement.style.display = 'block';
+
+    c_delay=0;
+
+    for(var j=0;j<array_size;j++)
+    {
+        div_update(divs[j],div_sizes[j],"yellow");//Color update
+
+        var key= div_sizes[j];
+        var i=j-1;
+        while(i>=0 && div_sizes[i]>key)
+        {
+            div_update(divs[i],div_sizes[i],"red");//Color update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Color update
+
+            div_sizes[i+1]=div_sizes[i];
+
+            div_update(divs[i],div_sizes[i],"red");//Height update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Height update
+    
+            div_update(divs[i],div_sizes[i],"blue");//Color update
+            if(i==(j-1))
+            {
+                div_update(divs[i+1],div_sizes[i+1],"yellow");//Color update
+            }
+            else
+            {
+                div_update(divs[i+1],div_sizes[i+1],"blue");//Color update
+            }
+            i-=1;
+        }
+        div_sizes[i+1]=key;
+
+        for(var t=0;t<j;t++)
+        {
+            div_update(divs[t],div_sizes[t],"green");//Color update
+        }
+    }
+    div_update(divs[j-1],div_sizes[j-1],"green");//Color update
+
+    enable_buttons();
+}
---
+++ scripts/merge_sort.js
@@ -0,0 +1,74 @@
+function Merge()
+{
+    //Setting Time complexities
+    document.getElementById("Time_Worst").innerText="O(N log N)";
+    document.getElementById("Time_Average").innerText="Θ(N log N)";
+    document.getElementById("Time_Best").innerText="Ω(N log N)";
+
+    //Setting Space complexity
+    document.getElementById("Space_Worst").innerText="O(N)";
+
+    // Setting details
+    document.getElementById("details").innerHTML="Merge sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array."
+
+    const linkElement = document.getElementById("reference")
+    linkElement.setAttribute('href', "https://www.geeksforgeeks.org/merge-sort/")
+    linkElement.style.display = 'block';
+
+    c_delay=0;
+
+    merge_partition(0,array_size-1);
+
+    enable_buttons();
+}
+
+function merge_sort(start,mid,end)
+{
+    var p=start,q=mid+1;
+
+    var Arr=[],k=0;
+
+    for(var i=start; i<=end; i++)
+    {
+        if(p>mid)
+        {
+            Arr[k++]=div_sizes[q++];
+            div_update(divs[q-1],div_sizes[q-1],"red");//Color update
+        }
+        else if(q>end)
+        {
+            Arr[k++]=div_sizes[p++];
+            div_update(divs[p-1],div_sizes[p-1],"red");//Color update
+        }
+        else if(div_sizes[p]<div_sizes[q])
+        {
+            Arr[k++]=div_sizes[p++];
+            div_update(divs[p-1],div_sizes[p-1],"red");//Color update
+        }
+        else
+        {
+            Arr[k++]=div_sizes[q++];
+            div_update(divs[q-1],div_sizes[q-1],"red");//Color update
+        }
+    }
+
+    for(var t=0;t<k;t++)
+    {
+        div_sizes[start++]=Arr[t];
+        div_update(divs[start-1],div_sizes[start-1],"green");//Color update
+    }
+}
+
+function merge_partition(start,end)
+{
+    if(start < end)
+    {
+        var mid=Math.floor((start + end) / 2);
+        div_update(divs[mid],div_sizes[mid],"yellow");//Color update
+
+        merge_partition(start,mid);
+        merge_partition(mid+1,end);
+
+        merge_sort(start,mid,end);
+    }
+}
